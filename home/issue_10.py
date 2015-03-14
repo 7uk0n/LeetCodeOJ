bc102eb5adeb76f5e61a1c9d8b9128854a413212
garfield <wuzehuai@gmail.com>
2015-03-14 17:48:24 +0800

    issue 10i

--- home/issue_10.py
+++ home/issue_10.py
@@ -1,38 +1,67 @@
+# coding=utf-8
+import time
+
 __author__ = 'garfield'
 
 
 class Solution:
     # @return a boolean
+    # method 1
+    # 带缓存的递归算法，不带缓存，效率非常低
+    def __init__(self):
+        self.count = 0
+        self.cache = {}
+
     def isMatch(self, s, p):
-        def matchone(ch, reg):
-            result = ch == reg or (reg == '.' and len(ch) > 0)
+        self.count += 1
+        if (s, p) in self.cache:
+            return self.cache[(s, p)]
+        if not s:
+            while len(p) > 1 and p[1] == '*':
+                p = p[2:]
+            result = not bool(p)
             return result
-        if not p and not s:
-            return not bool(s)
         if len(p) > 1 and p[1] == '*':
-            while matchone(s[0], p[0]):
-                # return True
-                s = s[1:]
-                if self.isMatch(s, p[2:]):
-                    return True
             if self.isMatch(s, p[2:]):
+                self.cache[(s, p)] = True
                 return True
-        elif len(s) > 0 and len(p) > 0:
-            if not matchone(s[0], p[0]):
+            elif s[0] == p[0] or p[0] == '.':
+                self.cache[(s, p)] = self.isMatch(s[1:], p)
+                return self.cache[(s, p)]
+            else:
+                self.cache[(s, p)] = False
                 return False
-            return self.isMatch(s[1:], p[1:])
-        return False
+        elif len(p) > 0:
+            if s[0] == p[0] or p[0] == '.':
+                self.cache[(s, p)] = self.isMatch(s[1:], p[1:])
+                return self.cache[(s, p)]
+            else:
+                self.cache[(s, p)] = False
+                return False
+        else:
+            self.cache[(s, p)] = False
+            return False
 
 
 
 if __name__ == '__main__':
     s = Solution()
-    print s.isMatch('aa','a')
-    print s.isMatch('aa','aaaaa')
-    print s.isMatch("abcd", "d*")
-    print s.isMatch("ab", ".*c")
+    # print s.isMatch('aa','aa')
+    print s.isMatch('aaa','a*a')
+    print s.isMatch('aaba','ab*a*c*a')
+
+    # print s.isMatch('aa','aaaaa')
+    # print s.isMatch("abcd", "d*")
+    # print s.isMatch("ab", ".*c")
     # print s.isMatch("aaa", "aa")
     # print s.isMatch("aa", "a*")
     # print s.isMatch("aa", ".*")
     # print s.isMatch("ab", ".*")
-    # print s.isMatch("aab", "c*a*b")
+    # print s.isMatch("aaa", "a*a")
+    # print s.isMatch("a", "ab*a")
+    # print s.isMatch("a", "ab*")
+    start = time.time()
+    print s.isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c")
+    print s.count
+    end = time.time()
+    print end-start
