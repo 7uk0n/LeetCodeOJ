a068b508f1896bd629b5500697c1a5a87e3b7c2d
garfield <wuzehuai@gmail.com>
2015-05-11 17:23:25 +0800

    issue_27 finished

--- home/issue_27.py
+++ home/issue_27.py
@@ -9,19 +9,22 @@ class Solution:
         if not nums:
             return 0
         head = tail = 0
-        while tail < len(nums) and nums[tail] == val:
+        for x in nums:
+            if not x == val:
+                break
             tail += 1
-        nums[head], nums[tail] = nums[tail], nums[head]
-        tail += 1
+        if tail > len(nums):
+            return 0
         for i in range(tail, len(nums)):
             if nums[i] != val:
-                nums[head + 1] = nums[i]
+                nums[head] = nums[i]
                 head += 1
-        return 0 if nums[0] == val else head + 1
+        return 0 if nums[0] == val else head
 
 
 if __name__ == '__main__':
     x = [1, 2, 2, 3, 4, 5, 1, 1, 1, ]
     y = [1]
     s = Solution().removeElement(y, 1)
+    print s
     print x[:s]
